5baf8af89f893af3011f310198bebbc43b51524c
logonrm <logonrm@fiap.com.br>
2017-11-27 22:31:02 -0200

    Primeiro Commit

---
+++ AleMacedo/Topico1-Assets/Assets/Scripts/Bloco.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bloco : MonoBehaviour {
+
+	void onCollisionEnter2D(Collision2D c){
+
+		if (c.gameObject.tag == "Piso") {
+			Destroy (c.gameObject);
+		}
+
+	}
+
+	void onCollisionStay2D(Collision2D c){
+	}
+
+	void onColisionExit2D(Collision2D c){
+	}
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ AleMacedo/Topico1-Assets/Assets/Scripts/Personagem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Personagem : MonoBehaviour {
+
+	public float velocidade;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		float move_x = Input.GetAxis ("Horizontal") * velocidade * Time.deltaTime;
+		float move_y = Input.GetAxis ("Vertical") * velocidade * Time.deltaTime;
+
+		transform.Translate (move_x, move_y, 0.0f);
+		if (Input.GetButtonDown ("Jump")) {
+			transform.position = Vector2.zero;
+		}
+	}
+}
